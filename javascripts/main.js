2d80780dc7d383b081cbe9361fcf065c5df7f3e7
ronency <ronency@users.noreply.github.com>
2014-02-18 18:09:15 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,17 @@
-console.log('hoverDelay.js is here!');
+
 $(function(){
   $('#demo-menu li').hoverDelay({
       delayIn: 200,
       delayOut:700,
       handlerIn: function($element){
+          console.log('in started');
           $element.animate({backgroundColor: '#FF0000'});  
+          console.log('in finished');
       },
       handlerOut: function($element){
+          console.log('out started');
           $element.animate({backgroundColor: '#0000FF'});  
+          console.log('out finished');
       }
   });  
 });
