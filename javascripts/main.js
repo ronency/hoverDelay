d4528eaf9c3729e261d5f6e87a795e1c3a4e1c75
ronency <ronency@users.noreply.github.com>
2014-02-18 17:55:57 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$(function(){
+  $('#demo-menu li').hoverDelay({
+      delayIn: 200,
+      delayOut:700,
+      handlerIn: function($element){
+          $element.animate({backgroundColor: '#FF0000'});  
+      },
+      handlerOut: function($element){
+          $element.animate({backgroundColor: '#0000FF'});  
+      }
+  });  
+});
