55b8f90c6a48f4774656ff9765acdbbc3681cd48
ronency <ronency@users.noreply.github.com>
2014-02-18 11:07:57 +0200

    fixed indentation

--- jquery.hoverDelay.js
+++ jquery.hoverDelay.js
@@ -9,19 +9,19 @@ $.fn.hoverDelay = function(options) {
 	return this.each(function() {
 		var timeoutIn, timeoutOut;
 		var $element = $(this);
-		$element.hover(
-			function() {
-			    if (timeoutOut){
-			        clearTimeout(timeoutOut);
-			    }
-			    timeoutIn = setTimeout(function(){options.handlerIn($element);}, options.delayIn);
-			},
-			function() {
-			    if (timeoutIn){
-			        clearTimeout(timeoutIn);
-			    }
-			    timeoutOut = setTimeout(function(){options.handlerOut($element);}, options.delayOut);
-			}
-		);
+			$element.hover(
+				function() {
+				    if (timeoutOut){
+				        clearTimeout(timeoutOut);
+				    }
+				    timeoutIn = setTimeout(function(){options.handlerIn($element);}, options.delayIn);
+				},
+				function() {
+				    if (timeoutIn){
+				        clearTimeout(timeoutIn);
+				    }
+				    timeoutOut = setTimeout(function(){options.handlerOut($element);}, options.delayOut);
+				}
+			);
     });
 };
