5e0edcdf618c139f1e2c86ed38ced8a321df30bb
ronen <ronencypis@gmail.com>
2016-05-08 14:23:00 +0300

    fixed indentation, added minified version

--- jquery.hoverDelay.js
+++ jquery.hoverDelay.js
@@ -2,8 +2,8 @@ $.fn.hoverDelay = function(options) {
 	var defaultOptions = {
 		delayIn: 300,
 		delayOut: 300,
-		handlerIn: function($element){},
-		handlerOut: function($element){}
+		handlerIn: function(){},
+		handlerOut: function(){}
 	};
 	options = $.extend(defaultOptions, options);
 	return this.each(function() {
@@ -11,16 +11,16 @@ $.fn.hoverDelay = function(options) {
 		var $element = $(this);
 			$element.hover(
 				function() {
-				    if (timeoutOut){
-				        clearTimeout(timeoutOut);
-				    }
-				    timeoutIn = setTimeout(function(){options.handlerIn($element);}, options.delayIn);
+			    if (timeoutOut){
+		        clearTimeout(timeoutOut);
+			    }
+			    timeoutIn = setTimeout(function(){options.handlerIn($element);}, options.delayIn);
 				},
 				function() {
-				    if (timeoutIn){
-				        clearTimeout(timeoutIn);
-				    }
-				    timeoutOut = setTimeout(function(){options.handlerOut($element);}, options.delayOut);
+			    if (timeoutIn){
+		        clearTimeout(timeoutIn);
+			    }
+			    timeoutOut = setTimeout(function(){options.handlerOut($element);}, options.delayOut);
 				}
 			);
     });
